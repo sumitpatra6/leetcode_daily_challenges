e055cc07ed7beed8a632d4f35567d02342fa9f6b
sumitpatra6 <sumitpatra6@gmail.com>
2021-05-29 21:04:37 +0530

    New solutions

--- max_product_of_word_lengths.py
+++ max_product_of_word_lengths.py
@@ -1,9 +1,31 @@
 def maxProducts(words):
-    hash_set = set([''.join(sorted(w)) for w in words])
-    hash_set = sorted(hash_set, key=lambda x: len(x))
-    print(hash_set)
+    def check(w1, w2):
+        w1 = set(w1)
+        w2 = set(w2)
+        for c in w1:
+            if c in w2:
+                return False
+        return True
+    max_result = 0
+    for i in range(len(words)):
+        for j in range(i + 1, len(words)):
+            word1 = sorted(words[i])
+            word2 = sorted(words[j])
+            if len(word1) < len(word2):
+                if not check(word1, word2):
+                    continue
+            else:
+                if not check(word2, word1):
+                    continue
+            print(word1, word2)
+            max_result = max(max_result, len(word1) * len(word2))
+    return max_result
 
 
-words = ["abcw","baz","foo","bar","xtfn","abcdef"]
+
+
+
+
+words = ["a","aa", "aaa", "aaaa"]
 result = maxProducts(words)
-print(words)
+print(result)
